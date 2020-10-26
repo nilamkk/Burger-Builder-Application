3a741380610408dcf6a7ffc052d7da216372cd29
Nilam Kummar Kalita <nilamkk54438@gmail.com>
2020-10-26 10:22:39 -0700

    On the way!

--- src/App.js
+++ src/App.js
@@ -1,85 +1,26 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios'
+import React ,{Component}from 'react';
+
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 
-import Pagination from './Pagination'
-import Pokelist from './PokeList'
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <Layout>
+                    <BurgerBuilder/>
+                </Layout>
 
-
-const APP=()=>{
-    const [pokemons,setPokemons]=useState([])
-    const [currentURL,setCurrentURL]=useState("https://pokeapi.co/api/v2/pokemon")
-    const [previousURL,setPreviousURL]=useState()
-    const [nextURL,setNextURL]=useState()
-    const [loading, setLoading] = useState(true)
-
-    useEffect(()=>{
-        setLoading(true)
-        let cancel
-        axios.get(currentURL,{
-            cancelToken: new axios.CancelToken(c=> cancel=c)
-        }).then(res=>{
-            setLoading(false)
-            setNextURL(res.data.next)
-            setPreviousURL(res.data.previous)
-            setPokemons(res.data.results.map(p=>p.name))
-        })
-
-        return ()=> cancel()
-    },[currentURL])
-
-
-    const gotoNext=()=>{
-        setCurrentURL(nextURL)
+            </div>
+        );
     }
-
-    const gotoPrev=()=>{
-        setCurrentURL(previousURL)
-    }
-
-    if(loading) return "LOADING..."
-    
-    
-    return(
-		<div>
-            <Pokelist
-            pokemons={pokemons}/>
-
-            <Pagination
-                nextBtn={nextURL? gotoNext:null}
-                prevBtn={previousURL? gotoPrev:null}
-            />
-		</div>
-	);	
 }
 
-export default APP;
+export default App;
 
 
 
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
-// export default App;
+
